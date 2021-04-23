08220f1ede16a2f1e094d131c661eda981fb1d1f
Gracie Zhou <yz6212@nyu.edu>
2021-04-23 13:32:35 +0800

    add some more track info

--- crawl.py
+++ crawl.py
@@ -19,23 +19,23 @@ offset = 0
 
 response = sp.playlist_items(pl_id,
                                 offset=offset,
-                                fields='items.track.uri,total',
-                                additional_types=['track'])
-print('response: ', response)
+                                fields='items.track.uri,items.track.name,items.track.artists.name,items.track.preview_url,total')
+
 tids = [item['track']['uri'] for item in response['items']]
+info = [{'artist_name':item['track']['artists'][0]['name'], 'track_name':item['track']['name'], 'preview_url':item['track']['preview_url']} for item in response['items']]
+
 
     
 
 features = sp.audio_features(tids)
-
-fieldnames = ['danceability','energy','key','loudness','mode','speechiness','acousticness','instrumentalness','liveness','valence','tempo','duration_ms','time_signature']
+fieldnames = ['id','danceability','energy','key','loudness','mode','speechiness','acousticness','instrumentalness','liveness','valence','tempo','duration_ms','time_signature','artist_name','track_name','preview_url']
 import csv
 with open('data.csv', 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    for feature in features:
+    for feature,info in zip(features, info):
+        feature.update(info)
         track = {key:feature[key] for key in fieldnames}
-        print('track: ',track)
         writer.writerow(track)
 
 
