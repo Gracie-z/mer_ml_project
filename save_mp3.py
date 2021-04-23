aaedb7dd8b9db3038b4ee91665af29a249ad8744
Gracie Zhou <yz6212@nyu.edu>
2021-04-23 14:22:37 +0800

    save mp3

---
+++ save_mp3.py
@@ -0,0 +1,18 @@
+from urllib.request import urlopen
+import pandas as pd
+import os
+
+
+df = pd.read_csv('data.csv',usecols=['id','preview_url'], keep_default_na=False)
+
+
+for index, id, url in df.itertuples():
+    if not url:
+        continue
+    print(url)
+    print(type(url))
+    html = urlopen(url)
+    file_dir = os.path.join('audio',id+'.mp3')
+    print(file_dir)
+    with open(file_dir, 'wb') as f:
+        f.write(html.read())
